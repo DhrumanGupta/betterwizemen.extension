d9939b2c66aa0a91ff614e017cadc9310683a2de
Dhruman Gupta <31363068+DhrumanGupta@users.noreply.github.com>
2021-12-03 03:39:23 +0530

    removed ajax, moved to fetch api

--- injections/noCaptcha.js
+++ injections/noCaptcha.js
@@ -1,12 +1,14 @@
 // ALL functions end with BetterWizemen just to prevent issues in other areas
 
-let button = $("#butLogin")
-const parent = button.parent()
-button.remove()
-button = parent.append('<button id="betterWizemenButton" type="button" style="width:100px;" class="btn btn-wizemen-primary btn-block btn-flat pull-right">Sign In</button>')
+let button;
+const changeButtonBetterWizemen = () => {
+	button = $("#butLogin")
+	const parent = button.parent()
+	button.remove()
+	button = parent.append('<button id="betterWizemenButton" type="button" style="width:100px;" class="btn btn-wizemen-primary btn-block btn-flat pull-right">Sign In</button>')
+}
 
-
-function validateEmailBetterWizemen(email) {
+const validateEmailBetterWizemen = (email) => {
 	return String(email)
 		.toLowerCase()
 		.match(
@@ -14,119 +16,93 @@ function validateEmailBetterWizemen(email) {
 		);
 };
 
-function onLoginErrorBetterWizemen(jqXHR, exception) {
-	var msg = '';
-	if (jqXHR.status === 0) {
-		msg = 'You are disconnected from your network.';
-	} else if (jqXHR.status == 404) {
-		msg = 'Requested page not found. [404]';
-	} else if (jqXHR.status == 500) {
-		msg = 'Your system cannot connect to Wizemen server.';
-	} else if (exception === 'parsererror') {
-		msg = 'Requested JSON parse failed.';
-	} else if (exception === 'timeout') {
-		msg = 'Your connection has timed out. Please check your connection.';
-	} else if (exception === 'abort') {
-		msg = 'Request aborted.';
-	} else {
-		msg = 'Something went wrong while logging you in.<br />Please try again after some time.';
-	}
-	hideWaitingMessage();
-	showMessageDialog("Oops !", msg, "error");
-	button.removeAttr("disabled");
-}
-
-function onLoginSuccessBetterWizemen(data, emailId, pwd, rememberMe) {
+async function onLoginSuccessBetterWizemen(data, emailId, pwd, rememberMe) {
 	if (data.length <= 0) {
 		hideWaitingMessage();
 		showMessageDialog("Sorry!", "Could not log you in.<br />Please get your user profile checked by the school ERP coordinator", "warning");
 		button.removeAttr("disabled");
+		return;
+	}
+
+	if (data.substring(0, 7) !== "success") {
+		hideWaitingMessage();
+		if (data.substring(0, 9) === "disabled:") {
+			$("#chkRememberMe").prop("checked", false);
+			let stat = data.substring(9);
+			if (stat.trim().length <= 0) {
+				stat = "Wizemen portal has currently been disabled for necessary updates.";
+			}
+			showMessageDialog("Wizemen Disabled", stat, "info");
+		} else if (data === "cantremember") {
+			showMessageDialog("Alert!", "You cannot remember an impersonated account", "warning");
+		} else if (data === "Invalid security Code entered") {
+			await loginBetterWizemen(emailId, pwd, rememberMe)
+		} else {
+			button.removeAttr("disabled");
+		}
 	} else {
-		if (data.substring(0, 7) !== "success") {
-			hideWaitingMessage();
-			if (data.substring(0, 9) === "disabled:") {
-				$("#chkRememberMe").prop("checked", false);
-				var stat = data.substring(9);
-				if (stat.trim().length <= 0) {
-					stat = "Wizemen portal has currently been disabled for necessary updates.";
-				}
-				showMessageDialog("Wizemen Disabled", stat, "info");
-			} else if (data === "cantremember") {
-				showMessageDialog("Alert !", "You cannot remember an impersonated account", "warning");
-			} else if (data === "Invalid security Code entered") {
-				loginBetterWizemen(emailId, pwd, rememberMe)
-					.then()
-			} else {
-				button.removeAttr("disabled");
+		const responsedata = data.split(':')[0];
+		const remember_me_token = data.split(':')[1];
+		if (remember_me_token == null || remember_me_token === "") {
+			try {
+				localStorage.removeItem("remember_me_token");
+			} catch (err) {
 			}
 		} else {
-			const responsedata = data.split(':')[0];
-			const remember_me_token = data.split(':')[1];
-			if (remember_me_token == null || remember_me_token === "") {
-				try {
-					localStorage.removeItem("remember_me_token");
-				} catch (err) {
-				}
-			} else {
-				try {
-					localStorage.setItem("remember_me_token", remember_me_token);
-				} catch (err) {
-				}
+			try {
+				localStorage.setItem("remember_me_token", remember_me_token);
+			} catch (err) {
 			}
-
-			if (responsedata === "success_Faculty") {
-				if (autoredirect === "yes") {
-					window.location.href = "/launchpadnew?portalpath=" + portalpath + "&pagepath=" + pagepath + "&autoredirect=" + autoredirect + "&param=" + param;
-					window.location.href = "/launchpadnew";
-				} else {
-					window.location.href = "/launchpadnew";
-				}
-			} else if (responsedata === "success_Student") {
-				if (autoredirect === "yes") {
-					window.location.href = "/launchpadnew?portalpath=" + portalpath + "&pagepath=" + pagepath + "&autoredirect=" + autoredirect + "&param=" + param;
-				} else {
-					window.location.href = "/launchpadnew";
-				}
-
-			} else if (responsedata === "success_Parent") {
-				if (autoredirect === "yes") {
-					window.location.href = "/launchpad?portalpath=" + portalpath + "&pagepath=" + pagepath + "&autoredirect=" + autoredirect + "&param=" + param;
-				} else {
-					launchParentPortalDirect();
-				}
-			} else if (responsedata === "success_Staff") {
-				if (autoredirect === "yes") {
-					window.location.href = "/launchpadnew?portalpath=" + portalpath + "&pagepath=" + pagepath + "&autoredirect=" + autoredirect + "&param=" + param;
-				} else {
-					window.location.href = "/launchpadnew";
-				}
-			} else if (responsedata === "success_Admin") {
-				if (autoredirect === "yes") {
-					window.location.href = "/launchpadnew?portalpath=" + portalpath + "&pagepath=" + pagepath + "&autoredirect=" + autoredirect + "&param=" + param;
-				} else {
-					window.location.href = "/launchpadnew";
-				}
-			} else if (responsedata === "fail_Admin") {
-				hideWaitingMessage();
-				showMessageDialog("Invalid Impersonation", "You cannot Impersonate an Admin.", "error");
-				button.removeAttr("disabled");
-			}
-
 		}
+
+		if (responsedata === "success_Faculty") {
+			if (autoredirect === "yes") {
+				window.location.href = "/launchpadnew?portalpath=" + portalpath + "&pagepath=" + pagepath + "&autoredirect=" + autoredirect + "&param=" + param;
+				window.location.href = "/launchpadnew";
+			} else {
+				window.location.href = "/launchpadnew";
+			}
+		} else if (responsedata === "success_Student") {
+			if (autoredirect === "yes") {
+				window.location.href = "/launchpadnew?portalpath=" + portalpath + "&pagepath=" + pagepath + "&autoredirect=" + autoredirect + "&param=" + param;
+			} else {
+				window.location.href = "/launchpadnew";
+			}
+
+		} else if (responsedata === "success_Parent") {
+			if (autoredirect === "yes") {
+				window.location.href = "/launchpad?portalpath=" + portalpath + "&pagepath=" + pagepath + "&autoredirect=" + autoredirect + "&param=" + param;
+			} else {
+				launchParentPortalDirect();
+			}
+		} else if (responsedata === "success_Staff") {
+			if (autoredirect === "yes") {
+				window.location.href = "/launchpadnew?portalpath=" + portalpath + "&pagepath=" + pagepath + "&autoredirect=" + autoredirect + "&param=" + param;
+			} else {
+				window.location.href = "/launchpadnew";
+			}
+		} else if (responsedata === "success_Admin") {
+			if (autoredirect === "yes") {
+				window.location.href = "/launchpadnew?portalpath=" + portalpath + "&pagepath=" + pagepath + "&autoredirect=" + autoredirect + "&param=" + param;
+			} else {
+				window.location.href = "/launchpadnew";
+			}
+		} else if (responsedata === "fail_Admin") {
+			hideWaitingMessage();
+			showMessageDialog("Invalid Impersonation", "You cannot Impersonate an Admin.", "error");
+			button.removeAttr("disabled");
+		}
+
 	}
+
 }
 
 const loadNewCaptchaBetterWizemen = async () => {
-	const getNewCaptcha = () => {
-		return $.ajax({
-			url: "/Home/getCaptchaImage",
-			type: 'GET',
-			cache: false,
-		});
-	}
 	try {
-		const resp = await getNewCaptcha()
-		$('#imgCaptcha').attr("src", resp);
+		const resp = await fetch('/Home/getCaptchaImage', {
+			cache: 'no-cache',
+		})
+		$('#imgCaptcha').attr("src", await resp.text());
 	} catch {
 	}
 }
@@ -149,24 +125,30 @@ async function loginBetterWizemen(emailId, pwd, rememberMe) {
 	const workerBetterWizemen = await getCaptchaWorkerBetterWizemen()
 
 	const captcha = await workerBetterWizemen.recognize(document.getElementById('imgCaptcha'), 'eng')
-	$.ajax({
-		type: "POST",
-		url: "/homecontrollers/login/validateUser",
-		data: JSON.stringify({
-			emailid: emailId,
-			pwd: pwd,
-			schoolCode: schoolCode,
-			schoolName: schoolName,
-			captcha: captcha.data.text.trim().replace(/\n/g, '').replace(/\b/g, ''),
-			rememberMe: rememberMe
-		}),
-		contentType: "application/json; charset=utf-8",
-		datatype: "json",
-		success: function (data) {
-			onLoginSuccessBetterWizemen(data, emailId, pwd, rememberMe)
-		},
-		error: onLoginErrorBetterWizemen
-	});
+
+	try {
+		const resp = await fetch('/homecontrollers/login/validateUser', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json; charset=utf-8'
+			},
+			body: JSON.stringify({
+				emailid: emailId,
+				pwd: pwd,
+				schoolCode: schoolCode,
+				schoolName: schoolName,
+				captcha: captcha.data.text.trim().replace(/\n/g, '').replace(/\b/g, ''),
+				rememberMe: rememberMe
+			})
+		})
+
+		const data = await resp.text()
+		await onLoginSuccessBetterWizemen(data.slice(1, -1), emailId, pwd, rememberMe)
+	} catch (e) {
+		hideWaitingMessage();
+		showMessageDialog("Oops!", 'Something went wrong. Please check your internet connection, or try again later. If the issue persists, please contact an administrator', "error");
+		button.removeAttr("disabled");
+	}
 }
 
 // taken from wizemen, very messy
@@ -214,4 +196,5 @@ function mainBetterWizemen() {
 
 }
 
+changeButtonBetterWizemen();
 mainBetterWizemen()
